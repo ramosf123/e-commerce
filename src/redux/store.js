1111b05521e53eb1bf65b83795b11526a324914e
Farid Ramos <framos@ICSMB244.local>
2020-07-06 13:52:51 -0500

    added redux, and implemented userReducer and userActions to header and app component

---
+++ src/redux/store.js
@@ -0,0 +1,13 @@
+import { createStore } from "redux";
+// import logger from "redux-logger";
+import rootReducer from "./root-reducer";
+
+// const middlewares = [logger];
+
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  //   applyMiddleware(...middlewares)
+);
+
+export default store;
