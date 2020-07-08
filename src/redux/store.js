d50ff466994c128a83bf3cfb2640dc04d2d5c397
Farid Ramos <framos@ICSMB244.local>
2020-07-07 20:52:03 -0500

    updated cart utils to group items in cart reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,15 @@
-import { createStore } from "redux";
-// import logger from "redux-logger";
+import { createStore, applyMiddleware, compose } from "redux";
+import logger from "redux-logger";
 import rootReducer from "./root-reducer";
 
-// const middlewares = [logger];
+const middlewares = [logger];
 
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  //   applyMiddleware(...middlewares)
+  compose(
+    applyMiddleware(...middlewares),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 );
 
 export default store;
