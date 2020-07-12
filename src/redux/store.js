ab5a1af1c7731251f3671c8611d0154b575ac971
Farid Ramos <framos@ICSMB244.local>
2020-07-11 23:20:30 -0500

    built out production version of the e-commerce site, hid redux logger in production build

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,14 +3,15 @@ import { persistStore } from "redux-persist";
 import logger from "redux-logger";
 import rootReducer from "./root-reducer";
 
-const middlewares = [logger];
+const middlewares = [];
+
+if (process.env.NODE_ENV === "development") {
+  middlewares = [logger];
+}
 
 export const store = createStore(
   rootReducer,
-  compose(
-    applyMiddleware(...middlewares),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  compose(applyMiddleware(...middlewares))
 );
 
 export const persistor = persistStore(store);
