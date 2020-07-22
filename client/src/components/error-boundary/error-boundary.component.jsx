014d9965c9a836fc171187a9b4fa769426868161
Farid Ramos <framos@ICSMB244.local>
2020-07-22 11:52:31 -0500

    optimized site with lazy loading, and React.memo

---
+++ client/src/components/error-boundary/error-boundary.component.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+
+import {
+  ErrorImageContainer,
+  ErrorImageOverlay,
+  ErrorImageText,
+} from "./error-boundary.styles";
+
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hasError: false,
+    };
+  }
+  static getDerivedStateFromError(error) {
+    //process the error
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, info) {
+    console.log(error);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <ErrorImageOverlay>
+          <ErrorImageContainer imageUrl={"https://i.imgur.com/yW2W9SC.png"} />
+          <ErrorImageText>Sorry this page is broken</ErrorImageText>
+        </ErrorImageOverlay>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
